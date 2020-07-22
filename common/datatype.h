23cb1fcf6d1c345e0427cb332a156fa415c1f342
Doraemon-h <52338965+Doraemon-h@users.noreply.github.com>
2020-07-22 21:39:36 +0800

    Update datatype.h

--- common/datatype.h
+++ common/datatype.h
@@ -53,7 +53,7 @@ struct Speed{
     double x;
     double y;
 };
-​
+
 //球的状态
 struct BallStatus {
     struct Speed v;
@@ -63,5 +63,43 @@ struct BallStatus {
     char name[20];
     //pthread_mutex_t mutex;
 };
+struct Score{
+    int red;
+    int blue;
+};
 
+//action value
+#define ACTION_KICK 0x01
+#define ACTION_CARRY 0x02
+#define ACTION_STOP 0x04
+#define ACTION_DFL 0x08
+
+struct Ctl{
+    int action;
+    int dirx;
+    int diry;
+    int strength;
+};
+
+//type的值
+#define FT_HEART 0x01
+#define FT_ACK 0x02
+#define FT_MSG 0x04  //私聊，队友之间
+#define FT_WALL 0x08
+#define FT_CTL 0x10
+#define FT_MAP 0x20 //场地数据
+#define FT_FIN 0x40
+#define FT_SCORE 0x80
+#define FT_GAMEOVER 0x100
+
+#define MAX_MSG 1024
+
+struct FootBallMsg {
+    int type;
+    int size;
+    int team;
+    char name[20];
+    char msg[MAX_MSG];
+    struct Ctl ctl;
+};
 #endif
